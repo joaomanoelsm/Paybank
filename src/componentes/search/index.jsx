c6901ae3966a59fd42492990d24df7c18fa2657f
João Manoel <joaom.developer@gmail.com>
2022-07-26 12:14:21 -0300

    salvo

--- src/componentes/search/index.jsx
+++ src/componentes/search/index.jsx
@@ -8,26 +8,29 @@ const Search = () => {
 
   return (
     <>
-      <section className='search__container'>
+      <section className='search'>
         <label htmlFor="">Type to search for your contact</label>
         <input onChange={ ({ target }) => setSearch(target.value)} value={ search } className='input' placeholder='Look for the contact' type="text" />
-        <img className='search__img' src={ searchIcon } alt="" />
-      </section>
-      <section className='search__list'>
-        <h2>Contact</h2>
-        <div className='search__contacts'>
-          <div className='contact__add'>
-            <img src={ addIcon } alt="" />
-            <span>Add contact</span>
+        <img src={ searchIcon } alt="" />
+        <div className='search__content'>
+          <h2>Contact</h2>
+          <div className='search__list'>
+            <div className='search__add__contact'>
+              <img src={ addIcon } alt="" />
+              <span>Add contact</span>
+            </div>
+            <div className='search__contacts'>
+              <ul>
+                { contacts.map( contact => {
+                return (
+                  <li onClick={ ({ target }) => setSearch( target.innerText )} className='contact' key={ contact }>
+                    <span>{ contact }</span>
+                  </li>
+                )
+                }) }
+              </ul>
+            </div>
           </div>
-          { contacts.map( contact => {
-            return (
-              <div onClick={ ({ target }) => setSearch( target.innerText )} className='contact' key={ contact }>
-                <img src="" alt="" />
-                <span>{ contact }</span>
-              </div>
-            )
-          }) }
         </div>
       </section>
     </>
