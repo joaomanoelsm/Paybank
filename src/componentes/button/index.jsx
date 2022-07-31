500f679e9cfcbc44d139e417c241b81181cbbb84
João Manoel <joaom.developer@gmail.com>
2022-07-31 12:48:02 -0300

    salvo

---
+++ src/componentes/button/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Button = () => {
+  return (
+    <button className='button-section'>
+        <button className='button-section__button'>Confirmar</button>
+    </button>
+  )
+}
+
+export default Button
